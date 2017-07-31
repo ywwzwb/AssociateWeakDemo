502c7039222af46ca809b57290d1322affe31eca
ywwzwb <ywwzwb@gmail.com>
2017-07-31 15:24:32 +0800

    weak associate object demo

--- AssociateWeakDemo/ViewController.m
+++ AssociateWeakDemo/ViewController.m
@@ -7,19 +7,51 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+@interface MyObject : NSObject
+@end
+
+@implementation MyObject
+@end
+
+@interface MyObject (Category)
+@property(nonatomic, weak) NSObject* weakvalue;
+
+@end
+
+
+
+@implementation MyObject (Category)
+void *weakValueKey = NULL;
+-(void)setWeakvalue:(NSObject *)weakvalue {
+    __weak typeof(weakvalue) weakObj = weakvalue;
+    typeof(weakvalue) (^block)() = ^(){
+        return weakObj;
+    };
+    objc_setAssociatedObject(self, weakValueKey, block, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+-(NSObject *)weakvalue {
+    id (^block)() = objc_getAssociatedObject(self, weakValueKey);
+    return block();
+}
+@end
 
 @interface ViewController ()
-
+@property(nonatomic, strong) MyObject *myObj;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.myObj = [MyObject new];
+    self.myObj.weakvalue = [NSObject new];
 }
 
-
+-(void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"weak value is: %@", self.myObj.weakvalue);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
